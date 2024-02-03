1332f3c921d03f850adb49aa466fe2d71e5cb55e
AvionShea <avionsheadesigns@gmail.com>
2024-02-03 01:12:35 -0500

    class-weekend-boring day class practice complete

--- JAVASCRIPT_Classes/class12-materials/class-weekend-boring/js/main.js
+++ JAVASCRIPT_Classes/class12-materials/class-weekend-boring/js/main.js
@@ -4,7 +4,13 @@ function check() {
 
   const day = document.querySelector('#day').value
 
-  //Conditionals go here
+  if (day == 'Tuesday' || day == 'Thursday') {
+    document.querySelector('#placeToSee').innerHTML = "Class Day!"
+  } else if (day == 'Saturday' || day == 'Sunday') {
+    document.querySelector('#placeToSee').innerHTML = "Weekend!"
+  } else {
+    document.querySelector('#placeToSee').innerHTML = "Boring Day"
+  }
 
 
 }
