47d67d1f71b278f1d1e654fece020ec306cc3236
AvionShea <avionsheadesigns@gmail.com>
2024-02-08 08:19:56 -0500

    completion of simple-function js file

--- JAVASCRIPT_Classes/class13-materials/simple-functions/js/main.js
+++ JAVASCRIPT_Classes/class13-materials/simple-functions/js/main.js
@@ -1,5 +1,14 @@
 //create a function that adds two numbers and alerts the sum
+function addNum(num1, num2) {
+    alert(num1 + num2);
+}
 
 //create a function that multiplys three numbers and console logs the product
+function multiplyThree(firstNum, secondNum, thirdNum) {
+    console.log(firstNum * secondNum * thirdNum);
+}
 
 //create a function that divides two numbers and returns the ???
+function divideTwo(numOne, numTwo) {
+    return numOne / numTwo;
+}
