11f7afb07d7fb49113208e47855ff9e6af02b53a
AvionShea <avionsheadesigns@gmail.com>
2024-02-08 08:28:39 -0500

    added js pseduo code for temperature-converter file

--- JAVASCRIPT_Classes/class13-materials/temperature-converter/js/main.js
+++ JAVASCRIPT_Classes/class13-materials/temperature-converter/js/main.js
@@ -1,1 +1,7 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+
+//user inputs value - input field in html
+//on click - even listener, 
+//function retrieves value, - .value()
+//does calculation, - calculation
+//displays new value in dom - innerHTML
