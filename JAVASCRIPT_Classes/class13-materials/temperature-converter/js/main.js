030159ad2f2f9d27f40531b05499ba7612dfc789
AvionShea <avionsheadesigns@gmail.com>
2024-03-01 21:35:02 -0500

    spelling correction

--- JAVASCRIPT_Classes/class13-materials/temperature-converter/js/main.js
+++ JAVASCRIPT_Classes/class13-materials/temperature-converter/js/main.js
@@ -1,7 +1,7 @@
 //Write your pseduo code first!
 
 //user inputs value - input field in html
-//on click - even listener, 
+//on click - event listener, 
 //function retrieves value, - .value()
 //does calculation, - calculation
 //displays new value in dom - innerHTML
