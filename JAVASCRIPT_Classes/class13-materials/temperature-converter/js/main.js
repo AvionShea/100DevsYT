2b221a4a7a01863efbaa987a7351f85a0c40ccbc
AvionShea <avionsheadesigns@gmail.com>
2024-03-01 23:58:20 -0500

    created celsius to fahrenheit converter

--- JAVASCRIPT_Classes/class13-materials/temperature-converter/js/main.js
+++ JAVASCRIPT_Classes/class13-materials/temperature-converter/js/main.js
@@ -5,3 +5,13 @@
 //function retrieves value, - .value()
 //does calculation, - calculation
 //displays new value in dom - innerHTML
+
+document.getElementById('convert').addEventListener('click', convertTemp);
+
+function convertTemp(){
+    let celsius = document.getElementById('temp').value;
+
+    let farhrenheit = (celsius * 9/5) + 32;
+
+    document.getElementById('temp-display').innerText = `${farhrenheit}°F`;
+};
