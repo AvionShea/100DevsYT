1f840a8ab41eb0194bed848eb63ec67146c58838
AvionShea <avionsheadesigns@gmail.com>
2024-03-07 20:37:45 -0500

    deleted js code and retyped

--- JAVASCRIPT_Classes/class13-materials/bachelor-code/3/js/main.js
+++ JAVASCRIPT_Classes/class13-materials/bachelor-code/3/js/main.js
@@ -1,11 +1,17 @@
-const contestants = document.querySelectorAll('.contestant');
+//selecting all elements with a class of contestant from html and placing them into a variable
+const contestant = document.querySelectorAll('.contestant');
 
-Array.from(contestants).forEach(element => element.addEventListener('click', checkForRose));
+//Array through each element, adding an event listener
+Array.from(contestant).forEach(element => element.addEventListener('click', checkForRose));
 
+//function with a parameter
 function checkForRose(click){
+	//if the clicked target contains the class of rose
 	if(click.target.classList.contains('rose')){
-		document.querySelector('#nikki').classList.toggle("hidden");
+		//select it from the DOM and toggle the class hidden
+		document.querySelector('#nikki').classList.toggle('hidden');
 	}else{
-		alert("Wrong!");
+		//else alert the selection is incorrect
+		alert("That's incorrect!")
 	};
 };
