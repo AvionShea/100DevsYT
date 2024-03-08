df20fc402b50614dda5d9d51c1395ed7f7dff16f
AvionShea <avionsheadesigns@gmail.com>
2024-03-07 21:02:05 -0500

    deleted and retyped bachelor-code 1 js

--- JAVASCRIPT_Classes/class13-materials/bachelor-code/1/js/main.js
+++ JAVASCRIPT_Classes/class13-materials/bachelor-code/1/js/main.js
@@ -1,7 +1,11 @@
-// When 'Final Rose (h2)' is clicked, only show Nikki
-document.querySelector('#finalRose').addEventListener('click', hide)
+//Challenge
+	//When "Final Rose"(h1) is clicked, only show the recipient of the rose (Nikki)
 
-function hide(){
-	document.querySelector('#claire').style.display = 'none';
-	document.querySelector('#sharleen').style.display = 'none';
+//Get DOM element and run function when clicked
+document.getElementById('finalRose').addEventListener('click', roseRecipient);
+
+//function that hides images other than rose recipient.
+function roseRecipient(){
+	document.getElementById("claire").style.display = 'none';
+	document.getElementById("sharleen").style.display = 'none';
 };
