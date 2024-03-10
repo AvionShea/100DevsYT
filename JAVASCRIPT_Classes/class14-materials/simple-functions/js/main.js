6524c30bf9a09214a4e4e7eb5e3603fb7306d010
AvionShea <avionsheadesigns@gmail.com>
2024-03-10 03:47:12 -0400

    completed simple-functions lesson-14

--- JAVASCRIPT_Classes/class14-materials/simple-functions/js/main.js
+++ JAVASCRIPT_Classes/class14-materials/simple-functions/js/main.js
@@ -1,5 +1,20 @@
 //create a function that adds two numbers and alerts the sum
+function addTwoNumbers(num1, num2){
+    let sum = num1 + num2;
+
+    alert(sum);
+}
 
 //create a function that multiplys three numbers and console logs the product
+function multiplyThreeNumbers(number1, number2, number3){
+    let product = number1 * number2 * number3;
+
+    console.log(product);
+}
 
 //create a function that divides two numbers and returns the ???
+function divideTwoNumbers(){
+    let division = numbers1 / numbers2;
+
+    return division;
+}
