dfb7195331e985a91d7e94f6aca39c373756f1ee
AvionShea <avionsheadesigns@gmail.com>
2024-03-09 21:32:55 -0500

    coded temp converter in lesson-14 folder

--- JAVASCRIPT_Classes/class14-materials/temperature-converter/js/main.js
+++ JAVASCRIPT_Classes/class14-materials/temperature-converter/js/main.js
@@ -1,1 +1,16 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+
+//"listen" for click of button
+document.querySelector("#submit").addEventListener('click', convertToFahrenheit)
+
+//run function
+function convertToFahrenheit(){
+    //grab Celsius value from html
+    let celsius = document.querySelector("#celsius").value;
+
+    //convert from Celsius to Fahrenheit °F = (°C × 9/5) + 32
+    let convertedTemp = (celsius * 9/5) + 32;
+
+    //display in DOM
+    document.querySelector("#display-temp").innerHTML = `${convertedTemp}°F`
+}
