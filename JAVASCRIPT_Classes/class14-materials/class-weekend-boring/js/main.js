213e315a34ffa8f23bb4445da5beffc69f31223a
AvionShea <avionsheadesigns@gmail.com>
2024-03-11 06:40:44 -0400

    handles capital letters

--- JAVASCRIPT_Classes/class14-materials/class-weekend-boring/js/main.js
+++ JAVASCRIPT_Classes/class14-materials/class-weekend-boring/js/main.js
@@ -4,12 +4,12 @@ document.querySelector('#check').addEventListener('click', check)
 
 function check() {
 
-  const day = document.querySelector('#day').value
+  const day = document.querySelector('#day').value.toLowerCase();
 
   if(day === "tuesday" || day === "thursday"){
     console.log("YOU HAVE CLASS")
   }else if( day === "saturday" || day === "sunday"){
-    console.log("Its The Weekend")
+    console.log("It's The Weekend")
   }else{
     console.log("BORING")
   }
