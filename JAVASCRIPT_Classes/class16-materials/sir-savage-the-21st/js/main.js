ce78e53e4a0c19d05593233f650c70e95684dda0
AvionShea <avionsheadesigns@gmail.com>
2024-05-01 17:44:12 -0400

    ...

--- JAVASCRIPT_Classes/class16-materials/sir-savage-the-21st/js/main.js
+++ JAVASCRIPT_Classes/class16-materials/sir-savage-the-21st/js/main.js
@@ -2,8 +2,10 @@
 function twentyOne() {
     for (let i = 0; i <= 20; i++) {
         console.log("21");
+
+        //Bonus can you make it print '21' 21 times to the dom?
         document.querySelector("#savageSays").innerText += " " + 21;
     };
 };
 twentyOne();
-//Bonus can you make it print '21' 21 times to the dom?
+
