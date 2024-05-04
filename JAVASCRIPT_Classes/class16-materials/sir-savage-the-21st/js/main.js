7330c4cdba342445c3bee171097018c0eede0228
AvionShea <avionsheadesigns@gmail.com>
2024-05-04 13:16:46 -0400

    ...

--- JAVASCRIPT_Classes/class16-materials/sir-savage-the-21st/js/main.js
+++ JAVASCRIPT_Classes/class16-materials/sir-savage-the-21st/js/main.js
@@ -1,6 +1,6 @@
 //Create a function that has a loop that prints '21' 21 times to the console and then call that function
 function twentyOne() {
-    for (let i = 0; i <= 20; i++) {
+    for (let i = 1; i <= 21; i++) {
         console.log("21");
 
         //Bonus can you make it print '21' 21 times to the dom?
