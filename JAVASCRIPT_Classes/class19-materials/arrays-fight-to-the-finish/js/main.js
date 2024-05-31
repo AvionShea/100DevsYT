7035825497c7d15e09c2d1a3095492ce16f19c31
AvionShea <avionsheadesigns@gmail.com>
2024-05-31 08:38:47 -0400

    ...

--- JAVASCRIPT_Classes/class19-materials/arrays-fight-to-the-finish/js/main.js
+++ JAVASCRIPT_Classes/class19-materials/arrays-fight-to-the-finish/js/main.js
@@ -1,5 +1,10 @@
 //Create an array of movie titles. Loop through the array and each element to the h2.
 let arrayOfMovies = ["Saw", "Lion King", "Dracula", "Smurfs", "Meet the Browns"];
+let list = document.querySelector("#display");
+
+for (let i = 0; i < arrayOfMovies.length; i++) {
+    list.innerText += arrayOfMovies[i]
+}
 
 
 //Create an array of numbers. Loop through the array and three to each number and replace the old number.
