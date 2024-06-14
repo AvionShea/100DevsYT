6a0e3fe101a6ea23f49ed64c662d37d6f951e3f2
AvionShea <avionsheadesigns@gmail.com>
2024-06-14 08:18:07 -0400

    code refactor

--- JAVASCRIPT_Classes/class16-materials/stop-snacking/js/main.js
+++ JAVASCRIPT_Classes/class16-materials/stop-snacking/js/main.js
@@ -1,7 +1,8 @@
 //Create a function that grabs the number of snacks from the input and tells you to stop that many times
 document.querySelector("#help").addEventListener('click', stopSnacking)
 function stopSnacking() {
-    let numberOfSnacks = document.querySelector("#snacks").value;
+    let numberOfSnacks = Number(document.querySelector("#snacks").value);
+    document.querySelector("#stops").innerText = ""
 
     for (let i = 0; i < numberOfSnacks; i++) {
         document.querySelector("#stops").innerText += " " + "Please stop eating!";
