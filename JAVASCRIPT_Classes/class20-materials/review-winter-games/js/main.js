8435e2b4b353de566c1037424fcc60d51a3c730b
AvionShea <avionsheadesigns@gmail.com>
2024-08-04 22:05:18 -0400

    added additional example to class 20 winter-game practice

--- JAVASCRIPT_Classes/class20-materials/review-winter-games/js/main.js
+++ JAVASCRIPT_Classes/class20-materials/review-winter-games/js/main.js
@@ -1,5 +1,6 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
 
+// forEach example
 function evenNumberArray(arr) {
     let newArray = [];
     arr.forEach((element) => {
@@ -10,6 +11,17 @@ function evenNumberArray(arr) {
 
     return newArray;
 };
+console.log(evenNumberArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
 
-console.log(evenNumberArray([1, 2, 3, 4, 5, 6, 7, 8, 10]));
 
+// for Loop example
+function evenArray(num) {
+    let evenNumArr = [];
+    for (let i = 1; i <= num.length; i++) {
+        if (i % 2 === 0) {
+            evenNumArr.push(i)
+        };
+    };
+    return evenNumArr
+};
+console.log(evenArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
