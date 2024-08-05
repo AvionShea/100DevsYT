a8ccfcce4390b9963175ff6b98066622b15e4097
AvionShea <avionsheadesigns@gmail.com>
2024-08-04 21:28:04 -0400

    work through problem for class 20

--- JAVASCRIPT_Classes/class20-materials/review-summer-games/js/main.js
+++ JAVASCRIPT_Classes/class20-materials/review-summer-games/js/main.js
@@ -1,5 +1,6 @@
 //Create a function that takes in an array of numbers. Multiply each number together and alert the product.
 
+//forEach Example
 function multiplyArray(arr) {
     let product = 1;
     arr.forEach((element) => product *= element);
@@ -7,3 +8,12 @@ function multiplyArray(arr) {
 };
 
 multiplyArray([10, 2, 3]);
+
+//for loop Example
+function mutltiArray() {
+    let product = 1
+    for (let i = 0; i < arr.length; i++) {
+        product *= arr[i];
+    };
+    alert(product);
+};
