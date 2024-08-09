0461c2a0af5da4ec2194eb23410aed8cbbdeee4f
AvionShea <avionsheadesigns@gmail.com>
2024-08-08 20:59:16 -0400

    work through of class 21 materials

--- JAVASCRIPT_Classes/class21-materials/alien-rangers-review/js/main.js
+++ JAVASCRIPT_Classes/class21-materials/alien-rangers-review/js/main.js
@@ -1,9 +1,26 @@
 //Arrays
 
-//Create and array of tv shows. Loop through and print each show to the console
+//Create an array of tv shows. Loop through and print each show to the console
 
-//Create and array of numbers
+let tvShows = ["Tales of the Crypt", "American Horror Stories", "The First 48"];
+
+tvShows.forEach((show) => {
+    console.log(show);
+});
+
+//Create an array of numbers
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
 //Return a new array of numbers that includes every even number from the previous Arrays
 
+let onlyEvens = (arr) => arr.filter(num => num % 2 === 0)
+console.log(onlyEvens(numbers))
+
 //Create a function that takes in an array of numbers
+function sumSecondLowAndHigh(arr) {
+    let sortedArr = arr.sort((a, b) => a - b);
+    alert(sortedArr[1] + sortedArr[sortedArr.length - 2]);
+}
+
+sumSecondLowAndHigh([12, 54, 34, 6, 8]);
 //Alert the sum of the second lowest and the second highest number
