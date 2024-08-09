d7f0aca7b771d811cd9c84723ffd66a2d58c018d
AvionShea <avionsheadesigns@gmail.com>
2024-08-08 21:32:07 -0400

    work through of class 21 materials

--- JAVASCRIPT_Classes/class21-materials/objects-zeo/js/main.js
+++ JAVASCRIPT_Classes/class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,20 @@
 //Create a stopwatch object that has four properties and three methods
+
+let stopwatch = {
+    shape: "round",
+    color: "black",
+    time: "12",
+    brand: "Garmin",
+};
+
+stopwatch.start = function () {
+    console.log("Starting");
+};
+
+stopwatch.stop = function () {
+    console.log("Stopping");
+};
+
+stopwatch.sayBrand = function () {
+    console.log(this.brand);
+};
