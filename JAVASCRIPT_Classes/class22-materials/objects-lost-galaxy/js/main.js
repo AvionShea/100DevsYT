c042e87d2df79696b40fdc68b178ce884ab59479
AvionShea <avionsheadesigns@gmail.com>
2024-08-14 23:47:43 -0400

    work through of class 22 materials

--- JAVASCRIPT_Classes/class22-materials/objects-lost-galaxy/js/main.js
+++ JAVASCRIPT_Classes/class22-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,21 @@
 //Create a mouse object that has four properties and three methods
+
+let mouse = {
+    brand: "Logitech",
+    color: "Pink",
+    size: "Small",
+    purpose: "Gaming",
+    model: "Lift",
+};
+
+mouse.rightClick = function () {
+    console.log("right click");
+};
+
+mouse.leftClick = function () {
+    console.log("left click");
+};
+
+mouse.scroll = function () {
+    console.log("Zoom");
+};
