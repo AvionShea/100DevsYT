31eb2c0cb8ed8e4ba5cab0c2f05e50bce0a53286
AvionShea <avionsheadesigns@gmail.com>
2024-08-15 00:28:25 -0400

    work through of class 22 materials

--- JAVASCRIPT_Classes/class22-materials/objects-street-fighter/js/main.js
+++ JAVASCRIPT_Classes/class22-materials/objects-street-fighter/js/main.js
@@ -1,1 +1,21 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+
+function MakeStreetFighter(name, punch, kick, block, specialMove) {
+    this.name = name;
+    this.punch = punch;
+    this.kick = kick;
+    this.block = block;
+    this.specialMove = specialMove;
+
+    this.greet = function () {
+        console.log("You don't want none of this!");
+    };
+    this.taunt = function () {
+        console.log(`You can handle my ${this.specialMove}!`)
+    };
+    this.dodge = function () {
+        console.log("Whooooop, missed me!");
+    };
+};
+
+let sky = new MakeStreetFighter("Sky", "high", "shin", "face", "cobra strike");
