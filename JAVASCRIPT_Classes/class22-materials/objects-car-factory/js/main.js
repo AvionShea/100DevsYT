0f71b7a658ece716987953a5b7fe24211baea6bb
AvionShea <avionsheadesigns@gmail.com>
2024-11-16 22:36:03 -0500

    completed  car factory task

--- JAVASCRIPT_Classes/class22-materials/objects-car-factory/js/main.js
+++ JAVASCRIPT_Classes/class22-materials/objects-car-factory/js/main.js
@@ -1,1 +1,43 @@
 //Create a car object that has four properties and three methods
+
+//Original constructor
+function MakeCar(carMake, carModel, carColor, numOfDoors) {
+    this.carMake = carMake,
+        this.carModel = carModel,
+        this.carColor = carColor,
+        this.numOfDoors = numOfDoors
+
+    this.carAlarm = function alarm() {
+        alert("ALERT! ALERT! ALERT!")
+    }
+
+    this.carDoorLock = function lockDoors() {
+        alert(`Locked all ${this.numOfDoors} doors!`)
+    }
+
+    this.honk = function honk() {
+        alert("BEEP BEEP MOTHERPUCKER!")
+    }
+}
+
+//Class constructor
+class MakeCar {
+    constructor(carMake, carModel, carColor, numOfDoors) {
+        this.carMake = carMake,
+            this.carModel = carModel,
+            this.carColor = carColor,
+            this.numOfDoors = numOfDoors
+
+        this.carAlarm = function alarm() {
+            alert("ALERT! ALERT! ALERT!")
+        }
+
+        this.carDoorLock = function lockDoors() {
+            alert(`Locked all ${this.numOfDoors} doors!`)
+        }
+
+        this.honk = function honk() {
+            alert("BEEP BEEP MOTHERPUCKER!")
+        }
+    }
+}
