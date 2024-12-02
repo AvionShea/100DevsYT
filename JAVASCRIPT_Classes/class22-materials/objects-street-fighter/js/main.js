1f0cbdaa5b9861534d10ae84f0b173b054ffbced
AvionShea <avionsheadesigns@gmail.com>
2024-12-02 15:35:14 -0500

    examples

--- JAVASCRIPT_Classes/class22-materials/objects-street-fighter/js/main.js
+++ JAVASCRIPT_Classes/class22-materials/objects-street-fighter/js/main.js
@@ -1,23 +1,61 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
 
 function MakeStreetFighter(name, punch, kick, block, specialMove) {
+  this.name = name;
+  this.punch = punch;
+  this.kick = kick;
+  this.block = block;
+  this.specialMove = specialMove;
+
+  this.greet = function () {
+    console.log("You don't want none of this!");
+  };
+  this.taunt = function () {
+    console.log(`You can't handle my ${this.specialMove}!`);
+  };
+  this.dodge = function () {
+    console.log("Whooooop, missed me!");
+  };
+}
+
+let sky = new MakeStreetFighter("Sky", "high", "shin", "face", "cobra strike");
+let moonlight = new MakeStreetFighter(
+  "Moonlight",
+  "throat",
+  "stomach",
+  "head",
+  "super saiyan"
+);
+let flair = new MakeStreetFighter(
+  "Flair Galore",
+  "gut",
+  "knee",
+  "neck",
+  "purple nurple"
+);
+
+class StreetFighter {
+  constructor(name, punch, kick, block, catchPhrase, specialMove) {
     this.name = name;
     this.punch = punch;
     this.kick = kick;
     this.block = block;
+    this.catchPhrase = catchPhrase;
     this.specialMove = specialMove;
 
-    this.greet = function () {
-        console.log("You don't want none of this!");
+    this.welcome = function () {
+      console.log(`Welcome to ${this.name}'s world!`);
     };
-    this.taunt = function () {
-        console.log(`You can't handle my ${this.specialMove}!`)
+    this.catchPhrase = function () {
+      console.log(`Psst. ${this.catchPhrase}!`);
     };
-    this.dodge = function () {
-        console.log("Whooooop, missed me!");
-    };
-};
+  }
+}
 
-let sky = new MakeStreetFighter("Sky", "high", "shin", "face", "cobra strike");
-let moonlight = new MakeStreetFighter("Moonlight", "throat", "stomach", "head", "super saiyan")
-let flair = new MakeStreetFighter("Flair Galore", "gut", "knee", "neck", "purple nurple")
+let sparkle = new StreetFighter(
+  "Sparkle",
+  "side",
+  "throat",
+  "You thought",
+  "backflip"
+);
