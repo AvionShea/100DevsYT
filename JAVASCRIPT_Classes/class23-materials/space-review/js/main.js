9dbfdab25571bb4473e56af2cc24f06e5550aeb8
AvionShea <avionsheadesigns@gmail.com>
2024-12-04 19:17:20 -0500

    completed class 23 super-review

--- JAVASCRIPT_Classes/class23-materials/space-review/js/main.js
+++ JAVASCRIPT_Classes/class23-materials/space-review/js/main.js
@@ -1,12 +1,25 @@
 //Arrays
 
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
+let sum = [5, 20, 10, 30];
+sum = sum.reduce((a, b) => a + b, 0);
+alert(sum);
 
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
+const squared = (arr) => arr.map((element) => element ** 2)
 
 //Create a function that takes string
 //Print the reverse of that string to the console
+const reversedStr = (str) => console.log(str.split("").reverse().join(""))
+
+
 
 //Create a function that takes in a string
 //Alert if the string is a palindrome or not
+const isPalindromeCheck = (str) => alert(str === str.split("").reverse().join(""))
+
+isPalindromeCheck("racecar")
+isPalindromeCheck("lol")
+isPalindromeCheck("leon noel")
+isPalindromeCheck("math")
