bc6889e41bf2fc22febaec116308eeb8b37b917c
AvionShea <avionsheadesigns@gmail.com>
2024-12-04 20:29:32 -0500

    completed class 23 objects-tony-hawk-pro-skater

--- JAVASCRIPT_Classes/class23-materials/objects-tony-hawk-pro-skater/js/main.js
+++ JAVASCRIPT_Classes/class23-materials/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,24 @@
 //Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+
+class ProSkater {
+    constructor(name, gender, boardColor, gear, specialMove) {
+        this.name = name
+        this.gender = gender
+        this.boardColor = boardColor
+        this.gear = gear
+        this.specialMove = specialMove
+
+        this.skateMove = function () {
+            console.log(`Don't make me use my ${this.specialMove}!`);
+        };
+        this.fight = function () {
+            console.log("high kick");
+        };
+        this.block = function () {
+            console.log("face block");
+        };
+    };
+};
+
+let bob = new ProSkater("Bob", "Male", "Pink", true, "Backflip");
+let summer = new ProSkater("Summer", "Female", "Peach", true, "Kickflip");
