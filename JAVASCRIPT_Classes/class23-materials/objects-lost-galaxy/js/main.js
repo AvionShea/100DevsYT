a922acb688f11be7a29c1460f952c1fdc65a5250
AvionShea <avionsheadesigns@gmail.com>
2024-12-04 19:54:35 -0500

    completed class 23 objects-lost-galaxy

--- JAVASCRIPT_Classes/class23-materials/objects-lost-galaxy/js/main.js
+++ JAVASCRIPT_Classes/class23-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,19 @@
 //Create a dog object that has four properties and three methods
+let dog = {}
+
+dog.color = "Black";
+dog.size = "Large";
+dog.breed = "Rottie";
+dog.paws = 4
+dog.bark = function () {
+    alert("Woof!")
+}
+dog.sit = function () {
+    console.log("sit")
+}
+dog.handShake = function () {
+    console.log("Hand Shake!")
+}
+dog.fetch = function () {
+    console.log("Zwoop!")
+}
