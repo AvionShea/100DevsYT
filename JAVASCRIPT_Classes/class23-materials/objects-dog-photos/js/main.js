b7f8caebdfe5b1fd7b249882dd765ab3a2a26df6
AvionShea <avionsheadesigns@gmail.com>
2024-12-08 00:07:31 -0500

    created class 23 objects-dog-photos answer

--- JAVASCRIPT_Classes/class23-materials/objects-dog-photos/js/main.js
+++ JAVASCRIPT_Classes/class23-materials/objects-dog-photos/js/main.js
@@ -1,5 +1,14 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
-
+//Answer
+fetch("https://dog.ceo/api/breeds/image/random")
+    .then(response => response.json()) //parse response as JSON
+    .then(data => {
+        console.log(data.message)
+        document.querySelector("img").src = data.message
+    })
+    .catch(error => {
+        console.log(`error ${error}`)
+    })
 
 // Example
 fetch("https://dog.ceo/api/breeds/image/random")
