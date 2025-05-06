ebd403b56b08928e11b0e0dcc868bfca0b5ed849
Avion C. <avionsheadesigns@gmail.com>
2025-05-05 20:29:20 -0400

    practice

--- JAVASCRIPT_Classes/class12-materials/class-weekend-boring/js/main.js
+++ JAVASCRIPT_Classes/class12-materials/class-weekend-boring/js/main.js
@@ -4,11 +4,13 @@ document.querySelector('#check').addEventListener('click', compareDays)
 //compare and display
 function compareDays() {
   const day = document.querySelector('#day').value.toLowerCase()
-  if (day === 'tuesday' || day === 'thursday') {
-    document.querySelector('#placeToSee').innerHTML = 'Class Day!'
-  } else if (day === 'saturday' || day === 'sunday') {
-    document.querySelector('#placeToSee').innerHTML = 'Weekend!'
+  if (day === "tuesday" || day === "thursday") {
+    document.querySelector("#placeToSee").innerHTML = "Class Day!!";
+  } else if (day == "saturday" || day === "sunday") {
+    document.querySelector("#placeToSee").innerHTML = "It's the weekend!!";
+  } else if (day === 'wednesday') {
+    document.querySelector("#placeToSee").innerHTML = "HUMP DAAAAAAAAAAAAAAAAAAAAY!"
   } else {
-    document.querySelector('#placeToSee').innerHTML = 'Boring Day!'
+    document.querySelector("#placeToSee").innerHTML = "BOOOOOOORRRRRRRINNNNG DAY!";
   }
 }
