7ef9d120e20058d23adc91d415b4f777e3aa88c1
Avion C. <avionsheadesigns@gmail.com>
2025-05-05 22:11:31 -0400

    practice

--- JAVASCRIPT_Classes/class12-materials/background-picker/js/main.js
+++ JAVASCRIPT_Classes/class12-materials/background-picker/js/main.js
@@ -1,36 +1,37 @@
-document.getElementById('purple').onclick = partyPurple
-document.getElementById('green').onclick = partyGreen
-document.getElementById('blue').onclick = partyBlue
-document.getElementById('red').onclick = partyRed
+document.getElementById("purple").onclick = partyPurple;
+document.getElementById("green").onclick = partyGreen;
+//document.getElementById("blue").onclick = partyBlue;
+document.getElementById("blue").addEventListener("click", partyBlue);
+document.getElementById("red").onclick = partyRed;
 document.getElementById("orange").onclick = partyOrange;
-document.getElementById('reset').onclick = resetColor
-
+document.getElementById("reset").onclick = reset;
 
 function partyPurple() {
-  document.querySelector('body').style.backgroundColor = 'rgb(108, 0, 112)'
-  document.querySelector('body').style.color = 'white'
+  document.querySelector("body").style.backgroundColor = "rgb(108, 0, 112)";
+  document.querySelector("body").style.color = "white";
 }
 
 function partyGreen() {
-  document.querySelector('body').style.backgroundColor = 'rgba(0,253,81,1)'
-  document.querySelector('body').style.color = 'black'
+  document.querySelector("body").style.backgroundColor = "rgb(0,253,81)";
+  document.querySelector("body").style.color = "black";
 }
 
 function partyBlue() {
-  document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
-  document.querySelector('body').style.color = 'black'
+  document.querySelector("body").style.backgroundColor = "rgb(0,254,255)";
+  document.querySelector("body").style.color = "black";
 }
 
 function partyRed() {
-  document.querySelector('body').style.backgroundColor = 'rgba(255, 0, 0, 1)';
-  document.querySelector('body').style.color = 'white';
+  document.querySelector("body").style.backgroundColor = "rgb(255, 0, 0)";
+  document.querySelector("body").style.color = " white";
 }
+
 function partyOrange() {
   document.querySelector("body").style.backgroundColor = "rgb(255, 175, 4)";
   document.querySelector("body").style.color = "black";
 }
 
-function resetColor() {
-  document.querySelector('body').style.backgroundColor = 'rgba(255, 255, 255, 1)';
-  document.querySelector('body').style.color = 'black';
+function reset() {
+  document.querySelector("body").style.backgroundColor = "rgb(255, 255, 255)";
+  document.querySelector("body").style.color = "black";
 }
