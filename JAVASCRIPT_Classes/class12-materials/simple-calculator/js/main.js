ecceb599edb2e561ccd4d65170c95513fd6c1c1e
Avion C. <avionsheadesigns@gmail.com>
2025-05-06 21:12:00 -0400

    practice and html change

--- JAVASCRIPT_Classes/class12-materials/simple-calculator/js/main.js
+++ JAVASCRIPT_Classes/class12-materials/simple-calculator/js/main.js
@@ -1,34 +1,38 @@
-let total = 0
+let total = 0;
 
-document.querySelector('#pumpkin').addEventListener('click', zeroOut);
-document.querySelector('#dominosPizza').addEventListener('click', addThree);
-document.querySelector('#zebra').addEventListener('click', addNine);
-document.querySelector('#cantThinkOfAnything').addEventListener('click', subTwo);
-document.querySelector('#subTen').addEventListener('click', subTen);
-document.querySelector('#multiplyTwo').addEventListener('click', multiplyTwo);
-
+document.getElementById("pumpkin").onclick = zeroOut;
+document.getElementById("dominosPizza").onclick = addThree;
+document.getElementById("zebra").addEventListener("click", addNine);
+document.querySelector("#cantThinkOfAnything").onclick = minusTwo;
+document.querySelector("#subTen").addEventListener("click", subTen);
+document.getElementById("multiplyFour").onclick = multiplyFour;
 
 function zeroOut() {
-  let total = 0;
-  document.querySelector('#placeToPutResult').innerHTML = total;
+  total = 0;
+  document.getElementById("placeToPutResult").innerHTML = total;
 }
+
 function addThree() {
-  total = total + 3;
-  document.querySelector('#placeToPutResult').innerHTML = total;
+  total += 3;
+  document.getElementById("placeToPutResult").innerHTML = total;
 }
+
 function addNine() {
-  total = total + 9;
-  document.querySelector('#placeToPutResult').innerHTML = total;
+  total += 9;
+  document.getElementById("placeToPutResult").innerHTML = total;
 }
-function subTwo() {
-  total = total - 2;
-  document.querySelector('#placeToPutResult').innerHTML = total;
+
+function minusTwo() {
+  total -= 2;
+  document.getElementById("placeToPutResult").innerHTML = total;
 }
+
 function subTen() {
-  total = total - 10;
-  document.querySelector('#placeToPutResult').innerHTML = total;
+  total -= 10;
+  document.getElementById("placeToPutResult").innerHTML = total;
 }
-function multiplyTwo() {
-  total = total * 2;
-  document.querySelector('#placeToPutResult').innerHTML = total;
+
+function multiplyFour() {
+  total *= 4;
+  document.getElementById("placeToPutResult").innerHTML = total;
 }
