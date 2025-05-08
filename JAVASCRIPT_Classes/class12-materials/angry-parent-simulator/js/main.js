754c04bbf71e68dab2b02e21c5055160e46618cb
Avion C. <avionsheadesigns@gmail.com>
2025-05-07 20:37:11 -0400

    practice

--- JAVASCRIPT_Classes/class12-materials/angry-parent-simulator/js/main.js
+++ JAVASCRIPT_Classes/class12-materials/angry-parent-simulator/js/main.js
@@ -1,17 +1,28 @@
-document.querySelector('#yell').addEventListener('click', run)
+document.getElementById("yell").onclick = yell;
 
-function run() {
-  const fName = document.querySelector('#firstName').value
-  const fMidName = document.querySelector('#firstMiddle').value
-  const lMidName = document.querySelector('#lastMiddle').value
-  const lName = document.querySelector('#lastName').value
+function yell() {
+  const fname = document.querySelector("#firstName").value;
+  const fMiddleName = document.querySelector("#firstMiddle").value;
+  const lMiddleName = document.querySelector("#lastMiddle").value;
+  const lname = document.querySelector("#lastName").value;
 
-  document.querySelector('#placeToYell').innerText = `${fName} ${fMidName} ${lMidName} ${lName}!`
-
-  //Add what you should be doing - conditionals go here
-
-  // document.querySelector('#placeToYell').innerText = `${fName} ${fMidName} ${lMidName} ${lName}`
+  document.getElementById("placeToYell").textContent = `${fname} ${fMiddleName} ${lMiddleName} ${lname}!!!!`
 }
+
+// document.querySelector('#yell').addEventListener('click', run)
+
+// function run() {
+//   const fName = document.querySelector('#firstName').value
+//   const fMidName = document.querySelector('#firstMiddle').value
+//   const lMidName = document.querySelector('#lastMiddle').value
+//   const lName = document.querySelector('#lastName').value
+
+//   document.querySelector('#placeToYell').innerText = `${fName} ${fMidName} ${lMidName} ${lName}!`
+
+//   //Add what you should be doing - conditionals go here
+
+//   // document.querySelector('#placeToYell').innerText = `${fName} ${fMidName} ${lMidName} ${lName}`
+// }
 
 // const synth = window.speechSynthesis;
 // document.querySelector('#yell').addEventListener('click', run)
