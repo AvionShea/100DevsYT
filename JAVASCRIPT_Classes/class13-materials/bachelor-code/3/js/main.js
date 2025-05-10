684ce62363f668523031d98802be4961a266ce39
Avion <avionsheadesigns@gmail.com>
2025-05-10 02:13:23 -0400

    practice

--- JAVASCRIPT_Classes/class13-materials/bachelor-code/3/js/main.js
+++ JAVASCRIPT_Classes/class13-materials/bachelor-code/3/js/main.js
@@ -1,17 +1,18 @@
 //selecting all elements with a class of contestant from html and placing them into a variable
-const contestant = document.querySelectorAll('.contestant');
+const contestant = document.querySelectorAll(".contestant");
 
 //Array through each element, adding an event listener
-Array.from(contestant).forEach(element => element.addEventListener('click', checkForRose));
+Array.from(contestant).forEach(element => element.addEventListener("click", checkForRose));
+
 
 //function with a parameter
-function checkForRose(click){
-	//if the clicked target contains the class of rose
-	if(click.target.classList.contains('rose')){
+function checkForRose(click) {
+	//check if the clicked target contains the class of rose
+	if (click.target.classList.contains("rose")) {
 		//select it from the DOM and toggle the class hidden
-		document.querySelector('#nikki').classList.toggle('hidden');
-	}else{
-		//else alert the selection is incorrect
-		alert("That's incorrect!")
-	};
-};
+		document.getElementById("nikki").classList.toggle("hidden");
+
+	} else {
+		alert("Oops! That's incorrect. Please try again!");
+	}
+}
