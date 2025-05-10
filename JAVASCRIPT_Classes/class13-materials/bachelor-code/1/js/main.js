6c02aad8d6f8236f6337f296ac92946aed0d6964
Avion <avionsheadesigns@gmail.com>
2025-05-10 00:00:33 -0400

    practice

--- JAVASCRIPT_Classes/class13-materials/bachelor-code/1/js/main.js
+++ JAVASCRIPT_Classes/class13-materials/bachelor-code/1/js/main.js
@@ -1,11 +1,11 @@
 //Challenge
-	//When "Final Rose"(h1) is clicked, only show the recipient of the rose (Nikki)
+//When "Final Rose"(h1) is clicked, only show the recipient of the rose (Nikki)
 
 //Get DOM element and run function when clicked
-document.getElementById('finalRose').addEventListener('click', roseRecipient);
+document.querySelector("#finalRose").onclick = roseRecipient;
 
 //function that hides images other than rose recipient.
-function roseRecipient(){
-	document.getElementById("claire").style.display = 'none';
-	document.getElementById("sharleen").style.display = 'none';
-};
+function roseRecipient() {
+	document.getElementById("claire").style.display = "none";
+	document.getElementById("sharleen").style.display = "none";
+}
